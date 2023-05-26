f55a43e265c742db803572595e91bacd388c2f6b
Andrew Lamb <andrew@nerdnetworks.org>
2023-05-26 09:02:05 -0400

    Start of an attempt to port serialization tests out of benchmarks

--- datafusion/proto/src/lib.rs
+++ datafusion/proto/src/lib.rs
@@ -27,3 +27,58 @@ pub use generated::datafusion as protobuf;
 
 #[cfg(doctest)]
 doc_comment::doctest!("../README.md", readme_example_test);
+
+
+#[cfg(test)]
+mod test {
+    use datafusion::{sql::{sqlparser::{parser::Parser, dialect::GenericDialect, ast::Statement}}, prelude::SessionContext};
+
+    use crate::bytes::{logical_plan_to_bytes, logical_plan_from_bytes};
+
+
+    /// Ensures that the plans for the TPCH queries can be correctly serialized
+    #[tokio::test]
+    async fn  tpch_roundtrip() {
+        // Read in the entire SQL as a string
+        let tpch_data = std::fs::read_to_string("./testdata/tpch.sql").unwrap();
+
+        println!("Parsing tpch data...");
+
+        let statements = Parser::new(&GenericDialect{})
+            .try_with_sql(&tpch_data)
+            .expect("Error tokenizing")
+            .parse_statements()
+            .expect("Error parsing");
+
+        let ctx = SessionContext::new();
+
+        // For each statement, run it against a context.
+        for statement in statements {
+            // Special case the querie to serde them
+            match statement {
+                Statement::Query(query) => {
+                    let query = query.to_string();
+
+                    // Ensure the plan is the same after serialization
+                    let plan = ctx.sql(&query).await.unwrap();
+                    let plan = plan.into_optimized_plan().unwrap();
+                    let bytes = logical_plan_to_bytes(&plan).unwrap();
+                    let plan2 = logical_plan_from_bytes(&bytes, &ctx).unwrap();
+                    let plan_formatted = format!("{}", plan.display_indent());
+                    let plan2_formatted = format!("{}", plan2.display_indent());
+                    assert_eq!(plan_formatted, plan2_formatted);
+
+                },
+                statement => {
+                    let statement = statement.to_string();
+                    println!("Running {statement}...");
+                    ctx.sql(&statement.to_string()).await.expect("error during execution");
+                }
+            }
+
+        }
+
+
+    }
+
+}
